93099ab42c3225272432c3da8942024153a5ffab
lastbattle <4586194+lastbattle@users.noreply.github.com>
2023-09-29 09:39:18 +0800

    ReactorType

---
+++ MapleLib/WzLib/WzStructure/Data/ReactorType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MapleLib.WzLib.WzStructure.Data {
+
+    [Serializable]
+    public enum ReactorType {
+        ActivatedByAnyHit = 0,
+        ActivatedLeftHit = 1,
+        ActivatedRightHit = 2,
+        ActivatedBySkill = 5,
+        ActivatedByHarvesting = 8,
+        ActivatedByTouch = 9,
+        ActivatedbyItem = 100,
+        UNKNOWN = -1,
+        AnimationOnly = 999 // Sits there and does nothing
+    }
+}
