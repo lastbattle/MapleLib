af195d89942a44916fc8869ff7673ee782b1e174
lastbattle <4586194+lastbattle@users.noreply.github.com>
2024-09-08 22:23:20 +0800

    refractoring prior commit

--- MapleLib/WzLib/WzStructure/Data/CharacterStructure/PetSkillFlag.cs
+++ MapleLib/WzLib/WzStructure/Data/CharacterStructure/PetSkillFlag.cs
@@ -3,37 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace MapleLib.WzLib.WzStructure.Data.CharacterStructure
 {
     [Flags]
     public enum PetSkillFlag
     {
-        PETSKILL_PickupMeso = 1 << 0,  // 1
-        PETSKILL_PICKUPITEM = 1 << 1,  // 2
-        PETSKILL_LONGRANGE = 1 << 2,   // 4
-        PETSKILL_DROPSWEEP = 1 << 3,   // 8
-        PETSKILL_IGNORE = 1 << 4,      // 16 (Ignore pickup of some items)
-        PETSKILL_PICKUPALL = 1 << 5,   // 32 (Pickup unlooted items)
-        PETSKILL_CONSUMEHP = 1 << 6,   // 64
-        PETSKILL_CONSUMEMP = 1 << 7,   // 128
-        PETSKILL_AUTOBUFF = 1 << 8,    // 256
-        PETSKILL_SMART = 1 << 9,       // 512
-        PETSKILL_GIANT = 1 << 10,      // 1024
-        PETSKILL_SHOP = 1 << 11,       // 2048
-        PETSKILL_NUM = 12  // This is not a flag, but represents the number of flags
+        PickupMeso = 1, // 1
+        PickupItem = 2, // 2
+        LongRange = 3, // 4
+        DropSweep = 4, // 8
+        Ignore = 5, // 16
+        PickupAll = 6, // 32
+        ConsumeHP = 7, // 64
+        ConsumeMP = 8, // 128
+        AutoBuff = 9, // 256
+        Smart = 10, // 512
+        Giant = 11, // 1024
+        Shop = 12, // 2048
     }
 
     public static class PetSkillFlagExtensions
     {
         public static int GetValue(this PetSkillFlag flag)
         {
-            return (int)flag;
+            return 1 << (int)flag;
         }
 
         public static bool Check(this PetSkillFlag flag, int value)
         {
-            return (value & (int)flag) == (int)flag;
+            return (value & GetValue(flag)) == GetValue(flag);
         }
     }
 }
