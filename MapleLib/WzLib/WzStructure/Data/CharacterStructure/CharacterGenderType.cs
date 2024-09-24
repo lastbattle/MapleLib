09538ea8ccede05a77c6fac29eeb53eec8c669d5
lastbattle <4586194+lastbattle@users.noreply.github.com>
2024-09-24 18:50:04 +0800

    Create CharacterGenderType.cs

---
+++ MapleLib/WzLib/WzStructure/Data/CharacterStructure/CharacterGenderType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapleLib.WzLib.WzStructure.Data.CharacterStructure
+{
+    /// <summary>
+    /// The gender val used in wz, and client
+    /// </summary>
+    public enum CharacterGenderType
+    {
+        Male = 0, 
+        Female = 1,
+        Both = 2, // or 'none'
+    }
+}
