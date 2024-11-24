a34f253052d2137e4ff469adce8ff36cbdc03910
lastbattle <4586194+lastbattle@users.noreply.github.com>
2024-11-24 13:51:34 +0800

    Update MapType.cs: ITCPreview

--- MapleLib/WzLib/WzStructure/Data/MapType.cs
+++ MapleLib/WzLib/WzStructure/Data/MapType.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MapleLib.WzLib.WzStructure.Data
+﻿namespace MapleLib.WzLib.WzStructure.Data
 {
     public enum MapType
     {
         RegularMap,
         MapLogin,
+        ITCPreview,
         CashShopPreview
     }
 }
