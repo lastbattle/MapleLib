4f979a25a31b4969692f5465881bae709b48f9d8
lastbattle <4586194+lastbattle@users.noreply.github.com>
2024-12-07 20:31:46 +0800

    Update PetSkillFlag.cs - fix pet skill flag

--- MapleLib/WzLib/WzStructure/Data/CharacterStructure/PetSkillFlag.cs
+++ MapleLib/WzLib/WzStructure/Data/CharacterStructure/PetSkillFlag.cs
@@ -1,27 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Documents;
 
 namespace MapleLib.WzLib.WzStructure.Data.CharacterStructure
 {
     [Flags]
     public enum PetSkillFlag
     {
-        PickupMeso = 1, // 1
-        PickupItem = 2, // 2
-        LongRange = 3, // 4
-        DropSweep = 4, // 8
-        Ignore = 5, // 16
-        PickupAll = 6, // 32
-        ConsumeHP = 7, // 64
-        ConsumeMP = 8, // 128
-        AutoBuff = 9, // 256
-        Smart = 10, // 512
-        Giant = 11, // 1024
-        Shop = 12, // 2048
+        PickupMeso = 0, // pet is always able to pickup meso
+        PickupItem = 1, 
+        LongRange = 2, 
+        DropSweep = 3, 
+        Ignore = 4, 
+        PickupAll = 5,
+        ConsumeHP = 6,
+        ConsumeMP = 7,
+        AutoBuff = 8,
+        Smart = 9,
+        Giant = 10,
+        Shop = 11,
     }
 
     public static class PetSkillFlagExtensions
@@ -37,3 +32,20 @@ namespace MapleLib.WzLib.WzStructure.Data.CharacterStructure
         }
     }
 }
+
+/* 566
+enum $8093A0CAEB5D767A25125BE9634A1621
+{
+  PETSKILL_PICKUPITEM = 0x1,
+  PETSKILL_LONGRANGE = 0x2,
+  PETSKILL_DROPSWEEP = 0x4,
+  PETSKILL_IGNORE = 0x8,
+  PETSKILL_PICKUPALL = 0x10,
+  PETSKILL_CONSUMEHP = 0x20,
+  PETSKILL_CONSUMEMP = 0x40,
+  PETSKILL_AUTOBUFF = 0x80,
+  PETSKILL_SMART = 0x100,
+  PETSKILL_GIANT = 0x200,
+  PETSKILL_SHOP = 0x400,
+  PETSKILL_NUM = 0xB,
+};*/
